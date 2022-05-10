1b7460c793926a9516a17cbbacf9d8d99d9d15ce
Muhammed Nasser Shalaby Alazaly <alazaly@outlook.com>
2022-05-10 02:25:21 +0200

    [ADD] icon image to module

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Hospital Management System',
+    'version': '1.0.0',
+    'sequence': '1',
+    'summary': 'Hospital Management System',
+    'description': 'Hospital Management System',
+    'category': '',
+    'author': 'Muhammad Nasser',
+    'website': '',
+    'license': 'LGPL-3',
+    'depends': ['base',
+                ],
+    'data': [],
+    'demo': [],
+    'application': True,
+    'installable': True,
+    'auto_install': False
+}
