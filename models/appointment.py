dd5248e668130888aa64061023a8fbbbe65b16d7
Muhammed Nasser Shalaby Alazaly <alazaly@outlook.com>
2022-05-15 19:29:53 +0200

    [ADD] related field

--- models/appointment.py
+++ models/appointment.py
@@ -8,11 +8,11 @@ class HospitalAppointment(models.Model):
 
     # Note: model name should be in first
     patient_id = fields.Many2one('hospital.patient', string="Patient Name", tracking=True)
-    # age = fields.Integer(string="Patient Age", tracking=True)
-    # gender = fields.Selection([('male', 'Male'),
-    #                            ('female', 'Female'),
-    #                            ('other', 'Other')
-    #                            ], string="Patient Gender", tracking=True)
+    age = fields.Integer(string="Patient Age", related='patient_id.age', readonly=True)
+    gender = fields.Selection([('male', 'Male'),
+                               ('female', 'Female'),
+                               ('other', 'Other')
+                               ], string="Patient Gender", related='patient_id.gender', readonly=True)
     appointment_time = fields.Datetime(string="Appointment Time", default=fields.Datetime.now)
     booking_date = fields.Date(string="Booking Date", default=fields.Date.context_today)
 
