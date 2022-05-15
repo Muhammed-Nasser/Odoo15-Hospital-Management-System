b8f6d89d8840995b5c54b6a084e80d44b3a83a86
Muhammed Nasser Shalaby Alazaly <alazaly@outlook.com>
2022-05-15 18:45:32 +0200

    [ADD] datetime and date field

--- models/appointment.py
+++ models/appointment.py
@@ -13,4 +13,6 @@ class HospitalAppointment(models.Model):
     #                            ('female', 'Female'),
     #                            ('other', 'Other')
     #                            ], string="Patient Gender", tracking=True)
+    appointment_time = fields.Datetime(string="Appointment Time")
+    booking_date = fields.Date(string="Booking Date")
 
