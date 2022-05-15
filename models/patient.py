d72e436f85d9fa18786ebfc6cd9ff149c6990e42
Muhammed Nasser Shalaby Alazaly <alazaly@outlook.com>
2022-05-15 20:42:19 +0200

    [ADD] compute field with function to auto compute the age of patient

--- models/patient.py
+++ models/patient.py
@@ -1,4 +1,7 @@
+from datetime import date
+
 from odoo import fields, models, api
+from odoo.exceptions import UserError
 
 
 class HospitalPatient(models.Model):
@@ -7,9 +10,18 @@ class HospitalPatient(models.Model):
     _description = 'Hospital Patient'
 
     name = fields.Char(string="Patient Name", tracking=True)
-    age = fields.Integer(string="Patient Age", tracking=True)
+    date_of_birth = fields.Date(string="Date of Birth", tracking=True)
+    age = fields.Integer(string="Patient Age", compute='_compute_age', store=True)
     gender = fields.Selection([('male', 'Male'),
                                ('female', 'Female'),
                                ('other', 'Other')
                                ], string="Patient Gender", tracking=True)
     active = fields.Boolean(string="Active", default=1, tracking=True)
+
+    @api.depends('date_of_birth')
+    def _compute_age(self):
+        for rec in self:
+            if rec.date_of_birth:
+                if rec.date_of_birth < date.today():
+                    rec.age = date.today().year - rec.date_of_birth.year
+                raise UserError("Sorry, Birth date not valid.")
