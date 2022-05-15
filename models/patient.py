8f4548c3722607803afe4d7ffdb4797868e4443f
Muhammed Nasser Shalaby Alazaly <alazaly@outlook.com>
2022-05-15 13:47:21 +0200

    [ADD] tracking fields

--- models/patient.py
+++ models/patient.py
@@ -6,10 +6,10 @@ class HospitalPatient(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = 'Hospital Patient'
 
-    name = fields.Char(string="Patient Name")
-    age = fields.Integer(string="Patient Age")
+    name = fields.Char(string="Patient Name", tracking=True)
+    age = fields.Integer(string="Patient Age", tracking=True)
     gender = fields.Selection([('male', 'Male'),
                                ('female', 'Female'),
                                ('other', 'Other')
-                               ], string="Patient Gender")
-    active = fields.Boolean(string="Active", default=1)
+                               ], string="Patient Gender", tracking=True)
+    active = fields.Boolean(string="Active", default=1, tracking=True)
