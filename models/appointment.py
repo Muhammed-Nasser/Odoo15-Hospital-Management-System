3d692d5f646f5a35553dbacc6a51fc8120101644
Muhammed Nasser Shalaby Alazaly <alazaly@outlook.com>
2022-05-15 18:39:32 +0200

    [FIX] appointment menu

--- models/appointment.py
+++ models/appointment.py
@@ -6,6 +6,7 @@ class HospitalAppointment(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = 'Hospital Appointment'
 
+    # Note: model name should be in first
     patient_id = fields.Many2one('hospital.patient', string="Patient Name", tracking=True)
     # age = fields.Integer(string="Patient Age", tracking=True)
     # gender = fields.Selection([('male', 'Male'),
